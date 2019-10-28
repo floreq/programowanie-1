3c5af798932bba1b8ad96b405344653c0c468fe3
Piotr Florczak <piotrflorczakk@gmail.com>
2019-10-28 13:50:27 +0100

    Lab_28-10-2019

---
+++ programowanie-1_28-10-2019/programowanie-1_28-10-2019/functions.h
@@ -0,0 +1,9 @@
+#pragma once
+const int nn = 10;
+// Deklaracja funkcji
+void displayArray(double tab[nn], int n);
+// n - rowCount, m - columnCount
+void displayArray(double tab[][nn], int n, int m);
+void clearArray(double tab[nn], int n);
+void clearArray(double tab[][nn], int n, int m);
+
