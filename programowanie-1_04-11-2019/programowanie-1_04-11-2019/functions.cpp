c2f8b8fdf57cf40eebcff784f19edd3b4d5e2f30
Piotr Florczak <piotrflorczakk@gmail.com>
2019-11-14 18:22:21 +0100

    Dodanie  sprawdzania możliwości przesunięcia prawo/lewo

--- programowanie-1_04-11-2019/programowanie-1_04-11-2019/functions.cpp
+++ programowanie-1_04-11-2019/programowanie-1_04-11-2019/functions.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "functions.h"
+#include <conio.h> // Potrzebny do _kbhit() i _getch()
 
 using namespace std;
 
@@ -20,12 +21,31 @@ void showBoard(tBoard tab) {
 void clearBoard(tBoard tab) {
 	for (int row = 0; row < rowCount - 1; row++) {
 		for (int cell = 1; cell < colCount - 1; cell++) {
-			tab[row][cell] = 0;
+			if (tab[row][cell] <= 1) {
+				tab[row][cell] = 0;
+			}
 		}
 	}
 }
 
-void insertBlock(tBoard tab, tBlockTypes blockTypes, const int type, int rotation, int iRow, int iColumn, const int l) {
+void insertBlock(tBoard tab, tBlockTypes blockTypes, const int type, int rotation, int iRow, int& iColumn, int& newIColumn, const int l) {
+	int canMove = iColumn == newIColumn ? 0 : 1; // Jezeli iColumn jest rowne newIColumn to nie ma potrzeby wykonywac sprawdzania
+	int i = 0;
+	while (canMove == 1 && i < l) {
+		for (int n = 0; n < l; n++) {
+			int bCell = blockTypes[type][rotation][i][n];
+			int boardCell = tab[i + iRow][n + newIColumn];
+			if (bCell != 0 && boardCell >= 5) {
+				canMove = 0;
+				newIColumn = iColumn;
+				break;
+			}
+		}
+		i++;
+	}
+	if (canMove == 1) {
+		iColumn = newIColumn;
+	}
 	for (int i = 0; i < l; i++) {
 		for (int n = 0; n < l; n++) {
 			int bCell = blockTypes[type][rotation][i][n];
@@ -37,13 +57,18 @@ void insertBlock(tBoard tab, tBlockTypes blockTypes, const int type, int rotatio
 }
 
 void keyInput(int& startColumn, int& rotation, const int l) {
-	char input = 'w';
+	char input = ' ';
+	// Sprawdzanie czy zostal wcisniety klawisz
+	if (_kbhit()) {
+		input = _getch(); // Wpisanie wcisnietego klawisza
+	}
+	// Przypisanie operacji klawiszy
 	switch (input) {
-	case 'a': 
-		if (startColumn+l < colCount) startColumn++;
-		break;
-	case 's':
+	case 'a':
 		if (startColumn > 0) startColumn--;
+		break;
+	case 's': 
+		if (startColumn+l < colCount) startColumn++;
 		break;
 	case 'q':
 		rotation > 0 ? rotation--: rotation = 3;
